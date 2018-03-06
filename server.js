e9ff908569c7af47702063229dbad52a13722a14
Kalindrov <34249113+Kalindrov@users.noreply.github.com>
2018-03-05 22:27:42 -0600

    updated fixes

--- server.js
+++ server.js
@@ -1,30 +1,23 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
-var PORT = process.env.PORT || 3000;
-
-var app = express();
-
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application/json
-app.use(bodyParser.json());
-
-// Set Handlebars.
+var methodOverride = require('method-override');
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+var app = express();
+app.use(express.static(__dirname + '/public'));
 
-// Import routes and give the server access to them.
-var routes = require("./controller/burgers_controller.js");
+app.use(bodyParser.urlencoded({ 
+    extended: false 
+}));
 
-app.use(routes);
+app.use(methodOverride('_method'));
+app.engine('handlebars', exphbs({ 
+    defaultLayout: 'main' 
+}));
+app.set('view engine', 'handlebars');
 
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
-});
+var routes = require("./controllers/burgers_controller.js");
+app.use('/',routes);
+
+var port = 3000;
+app.listen(port);
