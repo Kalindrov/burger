d0abc887f56c42f868c738edf3cfa04879d78e37
Kalindrov <34249113+Kalindrov@users.noreply.github.com>
2018-03-05 22:35:41 -0600

    updated schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -3,10 +3,10 @@ CREATE database burger_db;
 
 USE burger_db;
 
-CREATE TABLE burger
+CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
-	burger_name VARCHAR(100) NULL,
-	devoured BOOLEAN NULL,
+	burger_name VARCHAR(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
 PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,7 @@
-INSERT INTO burgers (id, burger_name, devoured) VALUES
-("1", "The Hamburgular", true),
-("2", "The Rainbow", false),
-("3", "The Devil's Biscuit", true),
-("4", "The Monster Veg", false),
-("5", "The Hellish Twister", false);
+USE burger_db;
+INSERT INTO burgers (burger_name, devoured) VALUES
+("Orion", true),
+("Beta", false),
+("Andromeda", true),
+("Sombero", false),
+("Centaurus A", false);
